63be517827919a4367843aa62c0d835895687ba8
Ted Kesgar <t.kesgar@live.com>
2020-01-29 06:04:03 +0700

    Add footer notes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,15 +5,21 @@ export default function MainNavbar(): React.ReactElement {
   return (
     <footer className="border-top border-light mt-5 px-1 py-5">
       <Container>
-        Original mock UI assets credits to{" "}
+        Created by{" "}
+        <a href="https://tkesgar.com" target="_blank" rel="noopener noreferrer">
+          Ted Kesgar
+        </a>
+        .
+        <br />
+        Credits to{" "}
         <a
           href="https://www.facebook.com/gymnastiar.romadhon/posts/2661226957329992"
           target="_blank"
           rel="noopener noreferrer"
         >
           Adam Miftakhul Arzaq
-        </a>
-        .
+        </a>{" "}
+        for the original mock UI assets PSD.
       </Container>
     </footer>
   );
